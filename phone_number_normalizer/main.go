dfef6444c4f71adecc60df1f7f766b1a30eaeb26
FR4NC1SC011 <fcootzg@gmail.com>
2021-01-11 19:59:49 -0700

    Added sqlite support to phone_number_normalizer

--- phone_number_normalizer/main.go
+++ phone_number_normalizer/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
+	"log"
+	"os"
 	"strings"
 
 	valid "github.com/asaskevich/govalidator"
-	_ "github.com/lib/pq"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 const (
@@ -15,6 +18,59 @@ const (
 )
 
 func main() {
+	log.Println("Creating sqlite-database.db...")
+	file, err := os.Create("sqlite-database.db")
+	check(err)
+	file.Close()
+	log.Println("sqlite-database.db created")
+
+	sqliteDatabase, _ := sql.Open("sqlite3", "./sqlite-database.db")
+	defer sqliteDatabase.Close()
+
+	createTable(sqliteDatabase)
+	insertStudent(sqliteDatabase, "Francisco", "(614) 1634 881")
+	insertStudent(sqliteDatabase, "Martin", "6141561325")
+	insertStudent(sqliteDatabase, "Javier", "614-123-4567")
+
+	displayStudents(sqliteDatabase)
+
+}
+
+func createTable(db *sql.DB) {
+	createStudentTableSQL := `CREATE TABLE client (
+		"idClient" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+		"name" TEXT,
+		"number" TEXT
+	  );`
+
+	log.Println("Create phone number table...")
+	statement, err := db.Prepare(createStudentTableSQL)
+	check(err)
+	statement.Exec()
+	log.Println("phone number table created")
+}
+
+func insertStudent(db *sql.DB, name string, number string) {
+	log.Println("Inserting client record")
+	insertStudentSQL := `INSERT INTO client(name, number) VALUES (?, ?)`
+	statement, err := db.Prepare(insertStudentSQL)
+	check(err)
+	_, err = statement.Exec(name, number)
+	check(err)
+}
+
+func displayStudents(db *sql.DB) {
+	row, err := db.Query("SELECT * FROM client ORDER BY name")
+	check(err)
+	defer row.Close()
+
+	for row.Next() {
+		var id int
+		var name string
+		var number string
+		row.Scan(&id, &name, &number)
+		log.Println("Student: ", name, " ", number)
+	}
 
 }
 
@@ -35,3 +91,9 @@ func normalize(number string) string {
 
 // re := regexp.MustCompile("\\D")
 // return re.ReplaceAllString(number, "")
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
